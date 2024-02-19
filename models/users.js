c348520b078c754863055492d20ea442f84cb297
ChiragSethi-1153 <chirag.1153@zenmonk.tech>
2024-02-19 10:51:36 +0530

    commit

--- models/users.js
+++ models/users.js
@@ -1,13 +1,19 @@
 const mongoose =  require('mongoose')
 
+const validateEmail = function(email) {
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return regex.test(email);
+  };
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
     },
     email: {
         type: String,
-        // unique: true,
-        required: true
+        unique: true,
+        required: true,
+        validate: [validateEmail, "Please enter a valid email"],
     },
     password: {
         type: String,
